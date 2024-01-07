2d0a3b0fde7cb7f36f69e32185422a44ea429530
voidmoai <voidmoai@gmail.com>
2024-01-07 12:44:43 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.bo.softtabstop = 4
+vim.cmd('set number') 
+require("vm.core")
+require("vm.lazy")
---
+++ lua/vm/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {	
+	"drewtempelmeyer/palenight.vim",
+	priority = 1000,
+	config = function()
+		vim.cmd('colorscheme palenight')
+		vim.cmd('set termguicolors')
+	end,
+}
