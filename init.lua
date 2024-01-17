33ce51bd960a3df7e8e08679f4f520a231ffe292
voidglxy <voidmoai@gmail.com>
2024-01-17 20:57:27 +0100

    changes

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.cmd('set number') 
 require("vm.core")
 require("vm.lazy")
