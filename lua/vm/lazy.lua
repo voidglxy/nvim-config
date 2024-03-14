b2fc61088256b9d1eb1237b81059d98c2d02c981
voidglxy <voidmoai@gmail.com>
2024-03-14 20:29:44 +0100

    lsp-zero

--- lua/vm/lazy.lua
+++ lua/vm/lazy.lua
@@ -13,3 +13,13 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("vm.plugins")
 require("lualine").setup()
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup()
